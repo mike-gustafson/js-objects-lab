e350fc0a6989500531b4e75e083f7f49822cf94c
Mike Gustafson <gustafson.m@gmail.com>
2024-10-06 21:13:03 -0700

    code's sloppy, but temp.js works correctly

--- temp.js
+++ temp.js
@@ -1,69 +1,90 @@
 const pokemon = require('./data.js')
 
 const game = {
-    party: [],
+    party: {
+        pokemon: [],
+        types: [],
+        findTypes: function () {
+            game.party.pokemon.forEach(pokemon => {
+                if (!game.party.types.includes(pokemon.type))
+                        game. party.types.push(pokemon.type)
+                })
+            }
+        },
+    pokemon: { 
+            starters: [],
+            types: [],
+            findStarters: 
+            function () {
+                this.starters = pokemon.filter((pokemon) => pokemon.starter)
+            },
+            findTypes:
+            function () {
+                pokemon.forEach(pokemon => {              
+                    if (!this.types.includes(pokemon.type))
+                        this.types.push(pokemon.type)
+                    })
+            }},
     gyms: [
-      { location: "Pewter City", completed: false, difficulty: 1 },
-      { location: "Cerulean City", completed: false, difficulty: 2 },
-      { location: "Vermilion City", completed: false, difficulty: 3 },
-      { location: "Celadon City", completed: false, difficulty: 4 },
-      { location: "Fuchsia City", completed: false, difficulty: 5 },
-      { location: "Saffron City", completed: false, difficulty: 6 },
-      { location: "Cinnabar Island", completed: false, difficulty: 7 },
-      { location: "Viridian City", completed: false, difficulty: 8 },
+        { location: "Pewter City", completed: false, difficulty: 1 },
+        { location: "Cerulean City", completed: false, difficulty: 2 },
+        { location: "Vermilion City", completed: false, difficulty: 3 },
+        { location: "Celadon City", completed: false, difficulty: 4 },
+        { location: "Fuchsia City", completed: false, difficulty: 5 },
+        { location: "Saffron City", completed: false, difficulty: 6 },
+        { location: "Cinnabar Island", completed: false, difficulty: 7 },
+        { location: "Viridian City", completed: false, difficulty: 8 },
     ],
     items: [
-      { name: "potion", quantity: 4 },
-      { name: "pokeball", quantity: 8 },
-      { name: "rare candy", quantity: 99 },
+        { 
+            name: "potion", 
+            quantity: 4 },
+        { 
+            name: "pokeball", 
+            quantity: 8 },
+        { 
+            name: "rare candy", 
+            quantity: 99 },
+    ],
+    changeQuantity: 
+            function (itemName, quantityChange) {
+                const item = game.items.find(i => i.name === itemName)
+                if ((item) && ((item.quantity + quantityChange) >= 0)) {
+                    item.quantity = item.quantity + quantityChange
+                } else {
+                    console.log(`There was an error changing the quantity of ${itemName} by ${quantityChange}`)
+                }
+        },
+    difficulty: [
+        {   options: ["Easy", "Medium", "Hard"]},
+        {   selectedDifficultyIdx: 1},
+        {   changeDifficulty:   
+            function (newDifficultyIdx) {
+                if (newDifficultyIdx < this.options.length) {
+                    this.selectedDifficultysIdx = newDifficultyIdx
+                    console.log(`Game difficulty has been set to ${this.options[this.selectedDifficultyIdx]}`)
+                } else {
+                    console.log(`Invalid difficulty level, plese try again`)
+                }
+        }}
     ],
   }
 
-/* 
-TASK: create game difficulty variables within the game object, then let the difficulty to the selected level
-INDENDED USE: present user with options based on option array, then update the selectedDifficultyIdx based on user input, 
-when the user changes or initially selects the difficulty, they should see the currrent set level (medium at at start of game),
-and use the change() function to set it to new value.
-*/
-game.difficulty = {
-    options : ["Easy", "Medium", "Hard"],
-    selectedDifficultyIdx : 1,
-    changeDifficulty : function (newDifficultyIdx) {
-        if (newDifficultyIdx < this.options.length) {
-            this.selectedDifficultysIdx = newDifficultyIdx
-            console.log(`Game difficulty has been set to ${this.options[this.selectedDifficultyIdx]}`)
-        } else {
-            console.log(`Invalid difficulty level, plese try again`)
-        }
-    }
-}
-
-
-// filter an array of starter pokemon within the pokemon object, then select a random one for the party
-pokemon.starters = pokemon.filter((pokemon) => pokemon.starter) 
-game.party.push(pokemon.starters[Math.floor(Math.random() * pokemon.starters.length)]) 
-console.log(`Greetings trainer!  You have chosen ${game.party[0].name} to accompany you on your adventure!`)
+game.pokemon.findStarters()
+game.party.pokemon.push(game.pokemon.starters[Math.floor(Math.random() * game.pokemon.starters.length)])
+game.party.findTypes()
+game.pokemon.findTypes()
+console.log(`Greetings trainer!  You have chosen ${game.party.pokemon[0].name} to accompany you on your adventure!`)
 console.log("")
 
 // create an array of all the types of pokemon. Array will be kept in the pokemon object.
-pokemon.types = []
-pokemon.forEach(element => {
-    if (!pokemon.types.includes(element.type))
-        pokemon.types.push(element.type)
-    })
 // create an array of all the types of pokemon in the party.  this is a function that can be reran whenever needed
-game.party.types = []
-game.party.updateTypes = function () {
-    game.party.forEach(element => {
-        if (!game.party.types.includes(element.type))
-            game. party.types.push(element.type)
-    })
-}
-game.party.updateTypes()
-console.log(`There are pokemon of the following type in the world: ${pokemon.types.join(', ')} types`)
-console.log(`You already have ${game.party.types.join(', ')} in your party so we'll find ${(4 - game.party.length)} new types for you.`)
+game.party.findTypes()
+console.log(`There are pokemon of the following type in the world: ${game.pokemon.types.join(', ')} types`)
+console.log(`You already have ${game.party.types.join(', ')} in your party so we'll find ${(4 - game.party.pokemon.length)} new types for you.`)
 
-let availablePokemonTypes = pokemon.types
+// create list of availiable pokemon types that are NOT already in the party, this will be a list of unique values
+let availablePokemonTypes = game.pokemon.types
 game.party.types.forEach(element => {
     for (let i=0; i<availablePokemonTypes.length; i++) {
         if (availablePokemonTypes[i] === element) {
@@ -86,15 +107,16 @@ for (let i=1; i<game.party.types.length; i++) {
             possiblePartyMembers.push(element)
         }
     });
-    game.party.push(possiblePartyMembers[Math.floor(Math.random() * possiblePartyMembers.length)])
+    game.party.pokemon.push(possiblePartyMembers[Math.floor(Math.random() * possiblePartyMembers.length)])
     }
 const partyMembers = []
-game.party.forEach(element => {
+game.party.pokemon.forEach(element => {
     partyMembers.push(element.name)
 })
 console.log("You're party is complete!  You're training with "+partyMembers.join(', ',"."))
 
 console.log("Wow! You're already strong enough to defeat the first two gyms!  LET'S GOOOOOOO!")
+
 game.gyms.completeUpToLevel = function (level) {
     game.gyms.forEach(gym => {
         if (gym.difficulty <  level) {
@@ -105,42 +127,42 @@ game.gyms.completeUpToLevel = function (level) {
 }
 game.gyms.completeUpToLevel(3)
 
-let starter = game.party[0].name
+let starter = game.party.pokemon[0].name
 console.log(starter, "is feeling weird.  He's Evolving!!!")
 for (let i=0; i<pokemon.length; i++) {
-    if (pokemon[i].number === game.party[0].number) {
-        game.party.shift()
-        game.party.unshift(pokemon[i+1])
+    if (pokemon[i].number === game.party.pokemon[0].number) {
+        game.party.pokemon.shift()
+        game.party.pokemon.unshift(pokemon[i+1])
         break
     }
 }
-console.log(starter, "has evolved into", game.party[0].name)
+console.log(starter, "has evolved into", game.party.pokemon[0].name)
 
 while (partyMembers.length > 0) {
     partyMembers.pop()
 }
-game.party.forEach(element => {
+game.party.pokemon.forEach(element => {
     partyMembers.push(element.name)
 })
 console.log("You're team is getting stronger!  You're now training with "+partyMembers.join(', ',"."))
 
 const starterNames = []
 
-pokemon.starters.forEach(element => {
+game.pokemon.starters.forEach(element => {
     starterNames.push(element.name)
 })
 
 console.log("You've come a long way since you chose between", starterNames.join(', ')+".")
 
 game.catchPokemon = function(pokemonObj) {
-    game.party.push(pokemonObj)
+    game.party.pokemon.push(pokemonObj)
     console.log(`You caught a ${pokemonObj.name}!`)
 
 }
 game.catchPokemon(pokemon[Math.floor(Math.random() * pokemon.length)])
 
 game.catchPokemon = function(pokemonObj) {
-    game.party.push(pokemonObj)
+    game.party.pokemon.push(pokemonObj)
     game.items.forEach(item =>{
         if (item.name === "pokeball") {
             item.quantity--
@@ -180,20 +202,20 @@ game.partyCount()
 
 game.gyms.completeUpToLevel(8)
 
-game.party.sort((a, b) => b.hp - a.hp)
+game.party.pokemon.sort((a, b) => b.hp - a.hp)
 game.collection = []
 
 game.catchPokemon = function(pokemonObj) {
     const pokeballs = game.items.find(i => i.name === "pokeball")
 
     if (pokeballs && pokeballs.quantity > 0){
-        if (game.party.length <= 6) {
-            game.party.push(pokemonObj)    
+        if (game.party.pokemon.length <= 6) {
+            game.party.pokemon.push(pokemonObj)    
         } else {
-            game.party.sort((a, b) => b.hp - a.hp)
-            if (game.party[6].hp < pokemonObj.hp) {
+            game.party.pokemon.sort((a, b) => b.hp - a.hp)
+            if (game.party.pokemon[6].hp < pokemonObj.hp) {
                 game.collection.push(game.party[6])
-                game.party[6] = pokemonObj
+                game.party.pokemon[6] = pokemonObj
             } else {
             game.collection.push(pokemonObj)
             }
@@ -219,12 +241,13 @@ game.catchPokemon(pokemon[Math.floor(Math.random() * pokemon.length)])
 game.catchPokemon(pokemon[Math.floor(Math.random() * pokemon.length)])
 game.catchPokemon(pokemon[Math.floor(Math.random() * pokemon.length)])
 
-game.party.forEach(pokemon => {
+game.party.pokemon.forEach(pokemon => {
     console.log(`You have a ${pokemon.hp} HP ${pokemon.name} in your party`)
 })
-
 game.collection.forEach(pokemon => {
-    console.log(`You have a ${pokemon.hp} HP ${pokemon.name} in your collection`)
+    if (pokemon) {
+        console.log(`You have a ${pokemon.hp} HP ${pokemon.name} in your collection`)
+    }
 })
 
 game.catchPokemon = function(pokemonObj) {
@@ -242,13 +265,7 @@ game.catchPokemon = function(pokemonObj) {
             game.collection.push(pokemonObj)
             }
         }
-
-        game.items.forEach(item =>{
-            if (item.name === "pokeball") {
-                item.quantity--
-                console.log(`You caught a ${pokemonObj.name}! You have ${item.quantity} pokeballs left.`)
-            }
-        })
+        game.changeQuantity('pokeballs', -1)
     } else {
         console.log(`You're out of pokeballs, you can't catch 'em all right now`)
     }
@@ -259,32 +276,17 @@ game.catchPokemon = function(name) {
     const pokemonName = name.toLowerCase()
     const pokemonObj = pokemon.find(i => i.name.toLowerCase() === pokemonName)
 
-    const pokeballs = game.items.find(i => i.name === "pokeball")
     if (pokemonObj) {
         console.log(`You requested information for "${name}":`)
-        if (pokeballs && pokeballs.quantity > 0){
-            if (game.party.length <= 6) {
-                game.party.push(pokemonObj)    
+        game.changeQuantity('pokeballs', -1);
+        game.party.pokemon.sort((a, b) => b.hp - a.hp)
+            if (game.party.pokemon[6].hp < pokemonObj.hp) {
+                game.collection.push(game.party[6])
+                game.party.pokemon[6] = pokemonObj
             } else {
-                game.party.sort((a, b) => b.hp - a.hp)
-                if (game.party[6].hp < pokemonObj.hp) {
-                    game.collection.push(game.party[6])
-                    game.party[6] = pokemonObj
-                } else {
                 game.collection.push(pokemonObj)
-                }
             }
-
-            game.items.forEach(item =>{
-                if (item.name === "pokeball") {
-                    item.quantity--
-                    console.log(`You caught a ${pokemonObj.name}! You have ${item.quantity} pokeballs left.`)
-                }
-            })
         } else {
-            console.log(`You found a ${pokemonObj.name}, but you're out of pokeballs so you can't catch 'em all right now`)
-        }
-    } else {
         console.log(`You requested information for "${name}":`)
         console.log(`Sorry, we couldn't find a pokemon named "${name}"`)
     }
@@ -293,13 +295,10 @@ game.catchPokemon = function(name) {
 game.catchPokemon('SnoRlax')
 game.catchPokemon('Yoda')
 
-pokemon.forEach(element => {
-    if (!pokemon.types.includes(element.type))
-        pokemon.types.push(element.type)
-})
-
 const pokemonByType ={}
-pokemon.types.forEach(element => pokemonByType[element] = [])
+game.pokemon.findTypes()
+
+game.pokemon.types.forEach(type => pokemonByType[type] = [])
 
 for (let i=0; i<pokemon.length; i++) {
     pokemonByType[pokemon[i].type].push(pokemon[i])
